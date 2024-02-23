b4a65cc94b4329619e5de80b507d378a5325a992
Betty <betty@Haleys-MacBook-Pro.local>
2024-02-23 10:39:30 -0800

    day2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 import React from 'react'
 
 const SetupPage = () => {
   return (
     <div className="p-4">
-      <Button>click</Button>
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
